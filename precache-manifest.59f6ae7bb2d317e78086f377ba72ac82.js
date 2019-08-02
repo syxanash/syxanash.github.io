4415f9c35c85dbafac19129883e1ddf9f2b0a02e
Simone Marzulli <smarzulli@guidewire.com>
2019-08-02 16:15:57 +0100

    Updates

---
+++ precache-manifest.59f6ae7bb2d317e78086f377ba72ac82.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be324694e870031fffa338de766c4fb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "585c9025d7848df4e69f",
+    "url": "/static/css/2.c9cf82b3.chunk.css"
+  },
+  {
+    "revision": "76b7f5d93712fb5f49d6",
+    "url": "/static/css/main.1530242e.chunk.css"
+  },
+  {
+    "revision": "585c9025d7848df4e69f",
+    "url": "/static/js/2.18b6d54b.chunk.js"
+  },
+  {
+    "revision": "76b7f5d93712fb5f49d6",
+    "url": "/static/js/main.6455bd5d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a42d6c14515bb1412e2ca4a8d641ea48",
+    "url": "/static/media/LICENSE.a42d6c14.txt"
+  },
+  {
+    "revision": "271ea5b7d8e06bc6e7b234738e0d9169",
+    "url": "/static/media/favicon.271ea5b7.png"
+  },
+  {
+    "revision": "4b77273192ff5016eb016f2bcbe2e24f",
+    "url": "/static/media/globe.4b772731.gif"
+  },
+  {
+    "revision": "21c4de2d12bf24e970ca13d8fbe335f3",
+    "url": "/static/media/insideyourcomputer.21c4de2d.jpg"
+  },
+  {
+    "revision": "58418e2f95582463866f64ed4006fc06",
+    "url": "/static/media/links.58418e2f.gif"
+  },
+  {
+    "revision": "3b54b0f94a403f76efc33b6bcaba8f4e",
+    "url": "/static/media/music.3b54b0f9.png"
+  },
+  {
+    "revision": "795b5ff9d27ed577cb49f71f72c1d77c",
+    "url": "/static/media/myavatar.795b5ff9.gif"
+  },
+  {
+    "revision": "e3f5cd304e830f21c88755a84ee6aadc",
+    "url": "/static/media/pgp.e3f5cd30.png"
+  },
+  {
+    "revision": "71bcb3869e8ec6da71193d5b700c7feb",
+    "url": "/static/media/poweroff.71bcb386.wav"
+  },
+  {
+    "revision": "08f44307b61d24e89f81847dc88ace29",
+    "url": "/static/media/public.08f44307.asc"
+  },
+  {
+    "revision": "e2a92c0c19155b5de67bee6558847338",
+    "url": "/static/media/screenmessage.e2a92c0c.gif"
+  },
+  {
+    "revision": "00f1239fe23e3ee65af4b70fdbfbd1bb",
+    "url": "/static/media/screenoff.00f1239f.gif"
+  }
+]);
