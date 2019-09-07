09b739df8e44fe20f2849c81c74051d742807547
Simone Marzulli <smarzulli@guidewire.com>
2019-09-07 16:27:30 +0100

    added Tron

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import './index.css';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
+console.info(`
+  << 🥏 >>
+
+ ⊂  .@.  ⊃
+  \\ (°° /
+   \\|T|/
+    |=|
+    / \\
+  _/   \\_
+
+     ~ Hi I'm Tron
+       and I fight for the users! ~
+`);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
