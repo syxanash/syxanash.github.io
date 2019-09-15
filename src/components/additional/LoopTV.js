6969b88c34e5f698c5816326763e6a24f932d5fe
Simone Marzulli <smarzulli@guidewire.com>
2019-09-15 17:02:41 +0100

    show static while image is loading

--- src/components/additional/LoopTV.js
+++ src/components/additional/LoopTV.js
@@ -4,6 +4,7 @@ import 'animate.css';
 
 import './LoopTV.css';
 import loopList from '../../resources/loops-list.json';
+import staticImage from '../../resources/images/static.gif';
 
 const loopImages = require.context('../../resources/images/loopTV', true);
 
@@ -19,6 +20,7 @@ class LoopTV extends Component {
       imageIndex: 0,
       disableTVOutput: false,
       disableCaption: false,
+      imageLoaded: false,
     };
   }
 
@@ -40,12 +42,17 @@ class LoopTV extends Component {
     this.setState({
       imageIndex: (imageIndex + 1) % imageList.length,
       disableCaption: false,
+      imageLoaded: false,
     });
+  }
+
+  imageLoaded = () => {
+    this.setState({ imageLoaded: true });
   }
 
   render() {
     const {
-      imageList, imageIndex, disableTVOutput, disableCaption,
+      imageList, imageIndex, disableTVOutput, disableCaption, imageLoaded,
     } = this.state;
     const { shouldPowerOn } = this.props;
 
@@ -63,7 +70,19 @@ class LoopTV extends Component {
 
     return (
       <div className='image-container' onClick={ this.changeLoopImage }>
-        <img className='main-image-loop' src={ loopImages(`./${imageList[imageIndex].img}`) } alt='main loop gif' />
+        <img
+          style={ { display: imageLoaded ? 'block' : 'none' } }
+          className='main-image-loop'
+          src={ loopImages(`./${imageList[imageIndex].img}`) }
+          alt='main loop gif'
+          onLoad={ this.imageLoaded }
+        />
+        <img
+          style={ { display: imageLoaded ? 'none' : 'block' } }
+          className='main-image-loop'
+          src={ staticImage }
+          alt='static animation'
+        />
         <div className={ `TV-output ${disableTVOutput ? 'animated fadeOut slower' : ''}` }>AV-1</div>
         <div className={ `loop-caption ${disableCaption ? 'animated fadeOut slower delay-5s' : ''}` }>{ imageList[imageIndex].description }</div>
       </div>
