1c5537635e3882721364bf96d216ff94dcbf0e04
syx <si.marzulli@gmail.com>
2020-03-10 16:00:07 +0000

    add new cursor images and minor fixes

--- src/components/additional/BrokenScreen.js
+++ src/components/additional/BrokenScreen.js
@@ -32,6 +32,16 @@ class BrokenScreen extends Component {
               -o-background-size: cover;
               background-size: cover;
               margin: 0px
+            }
+            * {
+              overflow: hidden;
+              cursor: url(data:image/gif;base64,R0lGODlhAQABAIABAAAAAAD/FywAAAAAAQABAAACAkQBADs=), auto;
+              webkit-touch-callout: none; /* iOS Safari */
+               -webkit-user-select: none; /* Safari */
+                -khtml-user-select: none; /* Konqueror HTML */
+                  -moz-user-select: none; /* Old versions of Firefox */
+                   -ms-user-select: none; /* Internet Explorer/Edge */
+                       user-select: none; /* Non-prefixed version, currently
             }`
           }
         </style>
--- src/components/additional/StoppedProgram.js
+++ src/components/additional/StoppedProgram.js
@@ -55,6 +55,16 @@ class Poweroff extends Component {
           {
             `body {
               background: url(data:image/gif;base64,R0lGODlhAQABAIABAAAAAAD/FywAAAAAAQABAAACAkQBADs=);
+            }
+            * {
+              overflow: hidden;
+              cursor: url(data:image/gif;base64,R0lGODlhAQABAIABAAAAAAD/FywAAAAAAQABAAACAkQBADs=), auto;
+              webkit-touch-callout: none; /* iOS Safari */
+               -webkit-user-select: none; /* Safari */
+                -khtml-user-select: none; /* Konqueror HTML */
+                  -moz-user-select: none; /* Old versions of Firefox */
+                   -ms-user-select: none; /* Internet Explorer/Edge */
+                       user-select: none; /* Non-prefixed version, currently
             }`
           }
         </style>
