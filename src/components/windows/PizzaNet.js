953ffbec6d56cf61642a5f0fc757a407c0d91d49
Simone Marzulli <smarzulli@guidewire.com>
2020-05-11 15:31:58 +0100

    created mvp for wizard UI

--- src/components/windows/PizzaNet.js
+++ src/components/windows/PizzaNet.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import ReactMarkdown from 'react-markdown';
 
+import {
+  Divider, Button, Progress,
+} from 'react95';
+
+import './PizzaNet.css';
 import pizzaIcon from '../../resources/icons/pizza.gif';
 
 class PizzaNetHeader extends Component {
@@ -14,10 +19,38 @@ class PizzaNetHeader extends Component {
 class PizzaNetBody extends Component {
   // eslint-disable-next-line arrow-body-style
   render = () => {
-    const input = '# This is a header\n\nAnd this is a paragraph';
+    const input = '# This is a header\n\nSed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliqui';
 
     return (
-      <ReactMarkdown source={ input } />
+      <div>
+        <div className='pizza-wizard-container'>
+          <div>
+            <img className='pizza-wizard-side-image' src='https://i.imgur.com/xKGBMwl.jpg' alt='pizza' />
+          </div>
+          <div className='pizza-wizard-text-container'>
+            <ReactMarkdown source={ input } />
+            <div style={ { display: 'block' } }>
+              <div style={ { paddingBottom: '5px' } }><span>Recipe progres...</span></div>
+              <div><Progress percent={ 52 } shadow={ false } /></div>
+            </div>
+          </div>
+        </div>
+        <Divider />
+        <div className='pizza-wizard-buttons-container'>
+          <Button
+            style={ { width: '120px' } }
+            size='lg'
+          ><b>&lt; <span className='underline-text'>B</span>ack</b></Button>
+          <Button
+            style={ { width: '120px' } }
+            size='lg'
+          ><b><span className='underline-text'>N</span>ext &gt;</b></Button>
+          <Button
+            style={ { marginLeft: '10px', width: '120px' } }
+            size='lg'
+          ><b>Cancel</b></Button>
+        </div>
+      </div>
     );
   }
 }
