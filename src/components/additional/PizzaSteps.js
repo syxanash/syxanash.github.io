67f453f75a39f8b36cd81a1ad016069745373823
Simone Marzulli <smarzulli@guidewire.com>
2020-05-12 11:58:19 +0100

    add javascript object for the steps

---
+++ src/components/additional/PizzaSteps.js
@@ -0,0 +1,40 @@
+const steps = {
+  introduction: `# Pizza Recipe
+  Hi I'm Simone! I'm not a professional pizzaiolo and I never had any sort of experience in a professional pizzeria restaurant. This recipe was developed based on some tips I acquired over time from lots and lots of resources and tutorials like youtube videos, family and friends recipes. I'm just an amateur who likes making pizza :)
+
+  If you plan to perfectionate your pizza making skills, I suggest to come up with your own recipe and research online watching lots of tutorials and different techniques. For now the following recipe is the one that works for me!`,
+  ingredients: `## Ingredients and quantities
+The Ingredients you need for this recipes are the following:
+  
+  * Strong white flour (the higher is the protein in the flour the better)
+  * Fresh or dry yeast (no instant dry yeast, just use a normal one)
+  * Salt
+  * Sugar
+  * Extra virgin olive oil
+  
+### Extra Info:
+  
+  * To convert from fresh yeast to active dry yeast, multiply the fresh quantity by **1/3**. Active dry yeast must be hydrated in warm water before being incorporated into a dough.
+  
+  * keep in mind that in order to do a **long fermentation** dough you will need a strong flour this means higher number of proteins, otherwise the dough won't resist the fermentation and when stretching it will easily break!
+  
+You can use the following web app to calculate the quantities (translate the website to English):
+  
+  [https://www.laconfraternitadellapizza.net/calcolapizza](https://www.laconfraternitadellapizza.net/calcolapizza/)
+  
+Alternatively you can also use this [Android application](https://play.google.com/store/apps/details?id=com.nicuz.calcolapizza&hl=en_IE)!
+  
+My typical quantities are for 4 pizza "Panielli" and 24 hours fermentation:
+  
+* 708gr of flour
+* 460gr of water
+* 23gr of salt
+* 9gr of oil
+* 0.61gr of yeast
+* 1 teaspoon of sugar
+
+That's it!
+  `,
+};
+
+export default steps;
--- src/components/windows/PizzaNet.js
+++ src/components/windows/PizzaNet.js
@@ -6,6 +6,7 @@ import {
 } from 'react95';
 
 import './PizzaNet.css';
+import pizzaSteps from '../additional/PizzaSteps';
 import pizzaIcon from '../../resources/icons/pizza.gif';
 
 class PizzaNetHeader extends Component {
@@ -19,8 +20,6 @@ class PizzaNetHeader extends Component {
 class PizzaNetBody extends Component {
   // eslint-disable-next-line arrow-body-style
   render = () => {
-    const input = '# This is a header\n\nSed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliqui';
-
     return (
       <div>
         <div className='pizza-wizard-container'>
@@ -28,12 +27,12 @@ class PizzaNetBody extends Component {
             <img className='pizza-wizard-side-image' src='https://live.staticflickr.com/65535/49882208456_8f708222a8_k.jpg' alt='pizza' />
           </div>
           <div className='pizza-wizard-text-container'>
-            <ReactMarkdown source={ input } />
+            <ReactMarkdown source={ pizzaSteps.ingredients } />
           </div>
         </div>
         <div className='pizza-wizard-progress'>
           <div style={ { display: 'block' } }>
-            <div style={ { paddingBottom: '5px' } }><span>Recipe progres...</span></div>
+            <div style={ { paddingBottom: '5px' } }><b>Recipe progress...</b></div>
             <div><Progress percent={ 0 } shadow={ false } style={ { maxWidth: '100%' } } /></div>
           </div>
         </div>
@@ -42,15 +41,15 @@ class PizzaNetBody extends Component {
           <Button
             style={ { width: '120px' } }
             size='lg'
-          ><b>&lt; <span className='underline-text'>B</span>ack</b></Button>
+          >&lt; <span className='underline-text'>B</span>ack</Button>
           <Button
             style={ { width: '120px' } }
             size='lg'
-          ><b><span className='underline-text'>N</span>ext &gt;</b></Button>
+          ><span className='underline-text'>N</span>ext &gt;</Button>
           <Button
             style={ { marginLeft: '10px', width: '120px' } }
             size='lg'
-          ><b>Cancel</b></Button>
+          >Cancel</Button>
         </div>
       </div>
     );
