9b890126a865d0c32e6459895e07f997d871052a
Simone Marzulli <smarzulli@guidewire.com>
2020-09-17 15:42:33 +0100

    improved konami code window popup

---
+++ src/themes/PippoRedmondDistracted.js
@@ -0,0 +1,41 @@
+const theme = {
+  hatchedBackground:
+    'url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAAECAYAAACp8Z5+AAAAIElEQVQYV2P8////fwYGBgZGRkZGMI0hABIFAbgEugAAQFQP/QfjEPcAAAAASUVORK5CYII=)',
+
+  canvas: '#ffffff',
+  material: '#ced0cf',
+  materialDark: '#9a9e9c',
+
+  borderDarkest: '#050608',
+  borderLightest: '#ffffff',
+  borderDark: '#888c8f',
+  borderLight: '#dfe0e3',
+
+  headerMaterialDark: '#636363',
+  headerMaterialLight: '#636363',
+  headerText: '#ffffff',
+
+  text: '#050608',
+  textInvert: '#ffffff',
+  textDisabled: '#888c8f',
+  textDisabledShadow: '#ffffff',
+
+  inputText: '#050608',
+  inputTextInvert: '#ffffff',
+  inputTextDisabled: '#888c8f',
+  inputTextDisabledShadow: '#ffffff',
+
+  tooltip: '#fefbcc',
+
+  anchor: '#1034a6',
+  anchorVisited: '#440381',
+
+  hoverBackground: '#000080',
+  checkmark: '#050608',
+  progress: '#000080',
+
+  flatLight: '#d8d8d8',
+  flatDark: '#9e9e9e',
+};
+
+export default theme;
