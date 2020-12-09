b882aa2a6acb99602805f728a280eabd7d9d1f1d
Simone Marzulli <smarzulli@guidewire.com>
2020-12-09 13:50:59 +0000

    improved style of foglio window

--- src/components/windows/Foglio.js
+++ src/components/windows/Foglio.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import {
-  Cutout,
+  Cutout, Button,
 } from 'react95';
 import ReactMarkdown from 'react-markdown/with-html';
 
+import Util from '../Util';
+
 import './Foglio.css';
 
+import rssIcon from '../../resources/icons/rss.png';
+import questionIcon from '../../resources/icons/question-mark.gif';
 import foglioIcon from '../../resources/icons/blog.gif';
 
 class FoglioHeader extends Component {
@@ -43,8 +47,30 @@ class FoglioBody extends Component {
     return (<React.Fragment>
       <Cutout className='foglio-cutout'>
         <div className='document-style'>
-          <p>{ postDate.toDateString() }</p>
           <ReactMarkdown source={ textDocument } escapeHtml={ false } />
+        </div>
+        <div style={ {
+          fontWeight: 'bold',
+          padding: '15px',
+          paddingTop: '0px',
+          textAlign: 'right',
+        } }><i>
+            <span>Published on { postDate.toDateString() }</span>
+          </i>
+        </div>
+      </Cutout>
+      <Cutout className='foglio-footer-cut-out'>
+        <div className='foglio-footer-buttons' style={ { float: 'right' } }>
+          <Button fullWidth>
+            <figcaption><b>What is this</b></figcaption>
+            <img src={ questionIcon } className='small-icon' alt="code"/>
+          </Button>
+        </div>
+        <div className='foglio-footer-buttons' style={ { float: 'left' } }>
+          <Button fullWidth onClick={ () => Util.openWebsiteURL({ url: 'https://themightybackend.herokuapp.com/rss.xml' }) }>
+            <img src={ rssIcon } className='small-icon' alt="code"/>
+            <figcaption>Feed RSS</figcaption>
+          </Button>
         </div>
       </Cutout>
     </React.Fragment>);
