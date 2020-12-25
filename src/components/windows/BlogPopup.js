6a4cdb2bf6f8998e4db4d3c2dae4c7956ebe64e4
Simone Marzulli <smarzulli@guidewire.com>
2020-12-25 11:30:04 +0000

    changed some stuff in Blog

--- src/components/windows/BlogPopup.js
+++ src/components/windows/BlogPopup.js
@@ -20,15 +20,11 @@ class BlogPopupBody extends Component {
         <Fieldset style={ { marginBottom: '20px', marginTop: '0' } }>
           <div style={ { marginTop: '-20px' } }>
             <h2>What is this?</h2>
-            <b>This is not exactly a blog</b>. But imagine this as a <i>One Post Blog</i>,
+            <b>This is not exactly a blog</b>, but more like a <i>one post blog</i>,
             a temporary single post which will be replaced from time
-            to time by a new one. You can keep yourself up to date using the Feed RSS.
-            <h2>Are you lazy?</h2>
-            <b>Generally yes</b>. I could've used some nice static blogging app to
-            make an actual blog, instead I'm spitting out one markdown file from a
-            lonely Heroku instance.
-            <h2>Is this a joke?</h2>
-            <b>Nope</b>, I genuinely liked the idea! Just think about it,
+            to time by a new one. You can keep yourself up to date using the  <a href='https://simonesmightybackend.herokuapp.com/rss.xml' rel='noopener noreferrer'>Feed RSS</a>.
+            <h2>But why?</h2>
+            I kind of liked the idea! Just think about it,
             so many cool websites and blogs went down in the last decade,
             though maybe something could still be
             found on <a href='https://archive.org' rel='noopener noreferrer'>archive.org</a> we don't really know if that'll go one day too.
