234a325ba69158c961f648fe7ca7cdcb81fb4888
Simone Marzulli <smarzulli@guidewire.com>
2021-01-06 17:20:31 +0000

    minor fixes

--- src/components/additional/StoppedProgram.js
+++ src/components/additional/StoppedProgram.js
@@ -69,16 +69,12 @@ class Poweroff extends Component {
           {
             `body {
               background: url(data:image/gif;base64,R0lGODlhAQABAIABAAAAAAD/FywAAAAAAQABAAACAkQBADs=);
+              background-color: black;
             }
             * {
               overflow: hidden;
               cursor: none;
-              webkit-touch-callout: none; /* iOS Safari */
-               -webkit-user-select: none; /* Safari */
-                -khtml-user-select: none; /* Konqueror HTML */
-                  -moz-user-select: none; /* Old versions of Firefox */
-                   -ms-user-select: none; /* Internet Explorer/Edge */
-                       user-select: none; /* Non-prefixed version, currently
+              pointer-events: none;
             }`
           }
         </style>
