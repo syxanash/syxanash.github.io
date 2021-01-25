dd0b04e714c98644c5b5f345445acaf8cd545385
Simone Marzulli <smarzulli@guidewire.com>
2021-01-25 20:41:57 +0000

    add sound effect when new user connects to lightbulb

--- src/components/additional/SoundEffects.js
+++ src/components/additional/SoundEffects.js
@@ -4,6 +4,7 @@ import poweroffSound from '../../resources/sounds/poweroff.wav';
 import errorSound from '../../resources/sounds/error.wav';
 import loopTVSound from '../../resources/sounds/looptv_chime.wav';
 import rebootSound from '../../resources/sounds/reboot.wav';
+import userOnline from '../../resources/sounds/online.wav';
 
 const SoundEffects = {
   errorSound: new Howl({
@@ -20,6 +21,10 @@ const SoundEffects = {
   }),
   rebootSound: new Howl({
     src: [rebootSound],
+    preload: false,
+  }),
+  userOnline: new Howl({
+    src: [userOnline],
     preload: false,
   }),
 };
