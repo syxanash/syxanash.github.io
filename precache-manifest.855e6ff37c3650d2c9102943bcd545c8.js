c609104e293ec1a9992f466af4d37af491100f39
Simone Marzulli <smarzulli@guidewire.com>
2022-04-12 09:15:16 +0100

    Updates

--- precache-manifest.855e6ff37c3650d2c9102943bcd545c8.js
+++ precache-manifest.855e6ff37c3650d2c9102943bcd545c8.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ef3f4501fb1ae7860ec5d9ceddc68053",
+    "revision": "60e5eebc8a5ffc7a7ad7489ea2fa92b3",
     "url": "/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.ad458773.chunk.css"
   },
   {
-    "revision": "a8dd2121e1741ebd8769",
-    "url": "/static/css/main.04cd8ac7.chunk.css"
+    "revision": "dcb8b5a8c4df83ff1d52",
+    "url": "/static/css/main.5f53b38a.chunk.css"
   },
   {
     "revision": "484d7f31341ef9771ddd",
     "url": "/static/js/2.7bbb01b1.chunk.js"
   },
   {
-    "revision": "a8dd2121e1741ebd8769",
-    "url": "/static/js/main.5637dad9.chunk.js"
+    "revision": "dcb8b5a8c4df83ff1d52",
+    "url": "/static/js/main.6b02cd70.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.00dbb58f92a1eccf34036f8ebad8e6f3.js"
+  "/precache-manifest.855e6ff37c3650d2c9102943bcd545c8.js"
 );
 
 self.addEventListener('message', (event) => {
