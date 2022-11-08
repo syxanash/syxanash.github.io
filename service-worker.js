9d86a46d5d6a5c8b3299e27a9470ba04599c297d
Simone Marzulli <smarzulli@guidewire.com>
2022-11-08 10:26:04 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.879e73f818a182c88b3f3e7219ed5fd3.js"
+  "/precache-manifest.33917f4f8879c3facbd0bc3712d2bbf6.js"
 );
 
 self.addEventListener('message', (event) => {
