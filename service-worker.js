5e027075b27a550602664d58114a7fd53cfe2ca1
syxanash <syxanash@gmail.com>
2023-01-09 11:52:35 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4ad24d87f09d851d14d40dfdfc713cb7.js"
+  "/precache-manifest.d31fcba9313b3f2b5c6dbf546500f071.js"
 );
 
 self.addEventListener('message', (event) => {
