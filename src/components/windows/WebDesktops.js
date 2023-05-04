17df7598f7b383cfb2fff00e35c70e3c6849b873
syxanash <syxanash@gmail.com>
2023-05-04 15:41:46 +0100

    removed support button in mobile layout

--- src/components/windows/WebDesktops.js
+++ src/components/windows/WebDesktops.js
@@ -144,7 +144,7 @@ class WebDesktopsBody extends Component {
           <Toolbar>
             <Button onClick={ this.openRandomURL } variant="menu"><img src={ hyperlinkIcon } alt='hyperlink' style={ { paddingRight: '4px' } } />Random</Button>
             <Button onClick={ () => Util.openWebsiteURL({ url: 'https://github.com/syxanash/awesome-web-desktops' }) } variant="menu"><img src={ sourceIcon } alt='hyperlink' style={ { paddingRight: '4px', height: '17px' } } />Contribute</Button>
-            <Button onClick={ () => Util.openWebsiteURL({ url: 'https://www.buymeacoffee.com/syxmone' }) } variant="menu"><img src={ pizzaSlice } alt='hyperlink' style={ { paddingRight: '7px' } } />Support</Button>
+            { Util.isMobile() ? null : <Button onClick={ () => Util.openWebsiteURL({ url: 'https://www.buymeacoffee.com/syxmone' }) } variant="menu"><img src={ pizzaSlice } alt='hyperlink' style={ { paddingRight: '7px' } } />Support</Button> }
             <Button onClick={ this.toggleShowHelp } active={ showHelp } variant="menu"><img src={ infoIcon } alt='info' style={ { paddingRight: '4px' } } />About</Button>
           </Toolbar>
         </div>
