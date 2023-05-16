d2ae2c16f823d4e97472f593eefc3219af0a479c
syxanash <syxanash@gmail.com>
2023-05-16 16:40:46 +0100

    Updates

--- precache-manifest.2f96ed52fe4f46fd04eb92e95dd06236.js
+++ precache-manifest.2f96ed52fe4f46fd04eb92e95dd06236.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a5e55d9b9e9c6ef0541608bb254fffdb",
+    "revision": "7485df9e347cb9832396ece12bd7a5da",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.ad458773.chunk.css"
   },
   {
-    "revision": "294bd8dacd2b3796a932",
+    "revision": "7a24c9589e92c4fb69f6",
     "url": "/static/css/main.b3affead.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.6d23f335.chunk.js"
   },
   {
-    "revision": "294bd8dacd2b3796a932",
-    "url": "/static/js/main.bff6e07a.chunk.js"
+    "revision": "7a24c9589e92c4fb69f6",
+    "url": "/static/js/main.7280efcc.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
