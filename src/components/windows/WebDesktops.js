efca5cd9f37b6a8d4fc1cae4c6b6406e68eb4b58
syxanash <syxanash@gmail.com>
2024-07-25 17:45:34 +0100

    minor fixes in icons

--- src/components/windows/WebDesktops.js
+++ src/components/windows/WebDesktops.js
@@ -69,7 +69,7 @@ class WebDesktopsBody extends Component {
       <div className='computer-icon'>
         <img style={ { height: '65px' } } src={ webDesktopsIcons(`./${icon}.gif`) } alt='single desktop icon' />
       </div>
-      <div className='website-favicon' style={ { left: icon === 'windows_new' ? '5px' : '1px' } }>
+      <div className='website-favicon' style={ { left: icon === 'windows_new' ? '5px' : '1px',  bottom: icon === 'windows11' ? '57px' : '' } }>
         <img style={ { height: '25px' } } src={ `https://s2.googleusercontent.com/s2/favicons?domain_url=${url}` } alt='computer icon' />
       </div>
       <div className='website-name'>
