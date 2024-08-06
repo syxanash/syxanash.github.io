e733204b4a3ef0e7feb41c6357f485d5edb8fffe
syxanash <syxanash@gmail.com>
2024-08-06 09:26:22 +0100

    fix hardcoded rule on filename :)

--- src/components/windows/WebDesktops.js
+++ src/components/windows/WebDesktops.js
@@ -135,7 +135,7 @@ class WebDesktopsBody extends Component {
       <div className='computer-icon'>
         <img style={ { height: '65px' } } src={ webDesktopsIcons(`./${icon}`) } alt='single desktop icon' />
       </div>
-      <div className='website-favicon' style={ { left: icon === 'windows_new' ? '7px' : '1px', bottom: icon === 'windows11' ? '57px' : '' } }>
+      <div className='website-favicon' style={ { left: icon === 'windows_new.png' ? '7px' : '1px', bottom: icon === 'windows11.gif' ? '57px' : '' } }>
         <img style={ { height: '25px' } } src={ `https://s2.googleusercontent.com/s2/favicons?domain_url=${url}` } alt='computer icon' />
       </div>
       <div className='website-name'>
