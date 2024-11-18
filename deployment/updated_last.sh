bd9236fdb8af479f33f48c237899fb961ce49577
syxanash <syxanash@gmail.com>
2024-11-18 17:36:04 +0000

    fix command in updated last.sh

--- deployment/updated_last.sh
+++ deployment/updated_last.sh
@@ -3,6 +3,6 @@
 CURRENT_DATE=$(date +"%Y-%m-%d")
 BUILD_HASH=$(md5 -q -s $(date +"%s"))
 BUILD_SIZE=$(du -hs build | awk '{print $1}')
-WD_UPDATE_DATE=$(GetFileInfo ./src/resources/remote-desktops.json | grep modified | awk '{print $2}')
+WD_UPDATE_DATE=$(git log -1 --pretty="format:%ci" ./src/resources/remote-desktops.json | awk '{print $1}')
 
 echo "{ \"date\": \"$CURRENT_DATE\", \"buildNumber\": \"$BUILD_HASH\", \"buildSize\": \"$BUILD_SIZE\", \"wdUpdate\": \"$WD_UPDATE_DATE\" }" > src/resources/last-updated.json
